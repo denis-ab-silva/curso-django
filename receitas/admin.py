faf9d51ded534dfd2a3d6944d1f69eb761f97d86
Denis Silva <denis.ab.silva@hotmail.com>
2021-04-16 20:13:27 -0300

    Incluindo filtros e categorias

--- receitas/admin.py
+++ receitas/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from .models import Receita
 class ListandoReceitas(admin.ModelAdmin):
-    list_display = ('id', 'nome_receita', 'categoria', 'tempo_preparo')
+    list_display = ('id', 'nome_receita', 'categoria', 'tempo_preparo', 'publicada', 'data_receita')
     list_display_links = ('id', 'nome_receita')
     search_fields = ('nome_receita',)
     list_filter = ('categoria',)
+    list_editable = ('publicada',)
     list_per_page = 10
 
 admin.site.register(Receita, ListandoReceitas)
