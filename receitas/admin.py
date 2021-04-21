ada9ad921a937437f9e801130dd4e40239576b9b
Denis Silva <denis.ab.silva@hotmail.com>
2021-04-20 22:40:24 -0300

    Autorização e melhorando o código

--- receitas/admin.py
+++ receitas/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from .models import Receita
+from .models import Pessoa
 class ListandoReceitas(admin.ModelAdmin):
-    list_display = ('id', 'nome_receita', 'categoria', 'tempo_preparo', 'publicada', 'data_receita')
+    list_display = ('id', 'nome_receita', 'categoria', 'tempo_preparo', 'publicada', 'pessoa', 'data_receita')
     list_display_links = ('id', 'nome_receita')
     search_fields = ('nome_receita',)
     list_filter = ('categoria',)
